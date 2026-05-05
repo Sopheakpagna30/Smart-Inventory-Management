d54295c90590203d0ea84254fab9a1b6d9848fe5
Sopheakpagna30 <haysopheakpagna12@gmail.com>
2026-05-05 17:52:50 +0700

    Add files via upload

---
+++ frontend/src/assets/images/CategoryFive/index.js
@@ -0,0 +1,14 @@
+import pic49 from "./Pic1.png";
+import pic50 from "./Pic2.png";
+import pic51 from "./Pic3.png";
+import pic52 from "./Pic4.png";
+import pic53 from "./Pic5.png";
+import pic54 from "./Pic6.png";
+import pic55 from "./Pic7.png";
+import pic56 from "./Pic8.png";
+import pic57 from "./Pic9.png";
+import pic58 from "./Pic10.png";
+import pic59 from "./Pic11.png";
+import pic60 from "./Pic12.png";
+
+export { pic49, pic50, pic51, pic52, pic53, pic54, pic55, pic56, pic57, pic58, pic59, pic60 };
