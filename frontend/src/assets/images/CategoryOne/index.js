1f19808556c7a0dab37b2d428b6b9d42d760ed43
Sopheakpagna30 <haysopheakpagna12@gmail.com>
2026-05-05 17:51:09 +0700

    Add files via upload

---
+++ frontend/src/assets/images/CategoryOne/index.js
@@ -0,0 +1,14 @@
+import pic1 from "./Pic1.png";
+import pic2 from "./Pic2.png";
+import pic3 from "./Pic3.png";
+import pic4 from "./Pic4.png";
+import pic5 from "./Pic5.png";
+import pic6 from "./Pic6.png";
+import pic7 from "./Pic7.png";
+import pic8 from "./Pic8.png";
+import pic9 from "./Pic9.png";
+import pic10 from "./Pic10.png";
+import pic11 from "./Pic11.png";
+import pic12 from "./Pic12.png";
+
+export { pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12 };
