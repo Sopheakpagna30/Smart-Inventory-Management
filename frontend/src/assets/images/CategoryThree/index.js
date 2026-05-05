92ee3c698c319e6f20e176d221f145fc72f8558b
Sopheakpagna30 <haysopheakpagna12@gmail.com>
2026-05-05 17:48:31 +0700

    Add files via upload

---
+++ frontend/src/assets/images/CategoryThree/index.js
@@ -0,0 +1,14 @@
+import pic25 from "./Pic1.png";
+import pic26 from "./Pic2.png";
+import pic27 from "./Pic3.png";
+import pic28 from "./Pic4.png";
+import pic29 from "./Pic5.png";
+import pic30 from "./Pic6.png";
+import pic31 from "./Pic7.png";
+import pic32 from "./Pic8.png";
+import pic33 from "./Pic9.png";
+import pic34 from "./Pic10.png";
+import pic35 from "./Pic11.png";
+import pic36 from "./Pic12.png";
+
+export { pic25, pic26, pic27, pic28, pic29, pic30, pic31, pic32, pic33, pic34, pic35, pic36 };
