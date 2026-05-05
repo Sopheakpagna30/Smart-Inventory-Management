44a26276884dff84f88639d6e6c1b969835402cf
Sopheakpagna30 <haysopheakpagna12@gmail.com>
2026-05-05 17:58:17 +0700

    Add files via upload

---
+++ frontend/src/components/common/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+// src/components/common/Loader.jsx
+import React from "react";
+import "./Loader.css";
+
+function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
