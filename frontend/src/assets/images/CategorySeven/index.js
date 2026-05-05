106fcead82fea6f6ed7809592c181c2722cb0d9d
Sopheakpagna30 <haysopheakpagna12@gmail.com>
2026-05-05 17:50:21 +0700

    Add files via upload

---
+++ frontend/src/assets/images/CategorySeven/index.js
@@ -0,0 +1,14 @@
+import pic73 from "./Pic1.png";
+import pic74 from "./Pic2.png";
+import pic75 from "./Pic3.png";
+import pic76 from "./Pic4.png";
+import pic77 from "./Pic5.png";
+import pic78 from "./Pic6.png";
+import pic79 from "./Pic7.png";
+import pic80 from "./Pic8.png";
+import pic81 from "./Pic9.png";
+import pic82 from "./Pic10.png";
+import pic83 from "./Pic11.png";
+import pic84 from "./Pic12.png";
+
+export { pic73, pic74, pic75, pic76, pic77, pic78, pic79, pic80, pic81, pic82, pic83, pic84 };
